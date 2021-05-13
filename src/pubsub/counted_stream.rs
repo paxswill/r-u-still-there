1d094736f6d0c029e968136e10b812f43693f085
Will Ross <paxswill@paxswill.com>
2021-05-13 15:46:09 +0000

    Add pubsub::CountedStream
    It's a thin wrapper over a Stream that also holds an owned CountToken.

---
+++ src/pubsub/counted_stream.rs
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+use futures::Stream;
+use pin_utils::unsafe_pinned;
+
+use std::fmt;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
+use crate::pubsub::CountToken;
+
+pub struct CountedStream<S: Stream + Send + Sync> {
+    token: CountToken,
+    stream: S,
+}
+
+impl<S: Stream + Send + Sync> CountedStream<S> {
+    unsafe_pinned!(stream: S);
+
+    pub fn new(token: CountToken, stream: S) -> Self {
+        Self { token, stream }
+    }
+}
+
+impl<S: Stream + Send + Sync> fmt::Debug for CountedStream<S>
+where
+    S: fmt::Debug,
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.debug_struct("CountedStream")
+            .field("token", &self.token)
+            .field("stream", &self.stream)
+            .finish()
+    }
+}
+
+impl<S: Stream + Send + Sync> Stream for CountedStream<S> {
+    type Item = S::Item;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        self.stream().poll_next(cx)
+    }
+}
+
+// Conditional Unpin to satisfy safety bounds of unsafe_pinned
+impl<S> Unpin for CountedStream<S>
+where
+    S: Stream + Send + Sync,
+    S: Unpin,
+{
+}
--- src/pubsub/mod.rs
+++ src/pubsub/mod.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 mod count_tree;
+mod counted_stream;
 pub mod spmc;
 
 pub use count_tree::{CountToken, TreeCount};
+pub use counted_stream::CountedStream;
 pub use spmc::Sender;
