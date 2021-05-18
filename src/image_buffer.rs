cab195a6a69cc089704481be763a33aa0acf17a7
Will Ross <paxswill@paxswill.com>
2021-05-18 18:17:36 +0000

    Clean up image_buffers.rs imports a bit

--- src/image_buffer.rs
+++ src/image_buffer.rs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 use bytes::Bytes;
-use image::{Luma, Rgba};
+use image::{ImageBuffer, Luma, Rgba};
 
-use std::vec::Vec;
-
-pub type ThermalImage = image::ImageBuffer<Luma<f32>, Vec<f32>>;
-pub type BytesImage = image::ImageBuffer<Rgba<u8>, Bytes>;
+pub type ThermalImage = ImageBuffer<Luma<f32>, Vec<f32>>;
+pub type BytesImage = ImageBuffer<Rgba<u8>, Bytes>;
