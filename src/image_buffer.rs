8abc213bc0d069b29d964b78d1b6939a5f576546
Will Ross <paxswill@paxswill.com>
2021-05-19 23:27:03 +0000

    Add doc comments for image_buffer type aliases

--- src/image_buffer.rs
+++ src/image_buffer.rs
@@ -2,5 +2,9 @@
 use bytes::Bytes;
 use image::{ImageBuffer, Luma, Rgba};
 
+/// Images where each point is a temperature in degrees Celsius.
 pub type ThermalImage = ImageBuffer<Luma<f32>, Vec<f32>>;
+
+/// Rendered raw images intended for viewing. A shared [bytes::Bytes] buffer is used to minimize
+/// copying.
 pub type BytesImage = ImageBuffer<Rgba<u8>, Bytes>;
