8f8d288d8742af5d72f9d892d106a099f190852a
Will Ross <paxswill@paxswill.com>
2021-06-18 13:48:10 +0000

    mqtt::State now async locks its value
    Making it easier to use in async closures, as now we can just clone it
    and use the clone later on, and since the value is now in a ref-counted
    mutex it's all good.

--- src/mqtt/state.rs
+++ src/mqtt/state.rs
@@ -1,14 +1,17 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 use std::fmt;
+use std::sync::Arc;
 
 use bytes::{BufMut, BytesMut};
 use rumqttc::{AsyncClient, QoS};
 use serde::Serialize;
+use tokio::sync::RwLock;
 use tracing::debug;
 
 /// A container for managing MQTT topic state.
+#[derive(Clone)]
 pub(crate) struct State<T> {
-    value: T,
+    value: Arc<RwLock<T>>,
     topic: String,
     retain: bool,
     qos: QoS,
@@ -17,19 +20,14 @@ pub(crate) struct State<T> {
 impl<T> State<T> {
     pub(crate) fn new<S>(value: T, topic: S, retain: bool, qos: QoS) -> Self
     where
-        S: Into<String>
+        S: Into<String>,
     {
         Self {
-            value,
+            value: Arc::new(RwLock::new(value)),
             topic: topic.into(),
             retain,
-            qos
+            qos,
         }
-    }
-
-    /// Get a reference to the current state of this topic
-    pub(crate) fn current(&self) -> &T {
-        &self.value
     }
 
     /// The full topic path for this state.
@@ -40,26 +38,36 @@ impl<T> State<T> {
 
 impl<T> State<T>
 where
-    T: Default
+    T: Clone,
 {
-    pub(crate) fn new_default_at<S>(topic: S) -> Self
-    where
-        S: Into<String>
-    {
-        Self::new(T::default(), topic, true, QoS::AtLeastOnce)
+    /// Get a copy of the current state of this topic
+    pub(crate) async fn current(&self) -> T {
+        self.value.read().await.clone()
     }
-
 }
 
 impl<T> State<T>
 where
-    T: PartialEq
+    T: Default,
+{
+    pub(crate) fn new_default_at<S>(topic: S) -> Self
+    where
+        S: Into<String>,
+    {
+        Self::new(T::default(), topic, true, QoS::AtLeastOnce)
+    }
+}
+
+impl<T> State<T>
+where
+    T: PartialEq,
 {
     /// Update the state of this topic, returning whether or not the new value is different than
     /// the old one.
-    pub(crate) fn update(&mut self, value: T) -> bool {
-        if self.value != value {
-            self.value = value;
+    pub(crate) async fn update(&mut self, new_value: T) -> bool {
+        let mut current_value = self.value.write().await;
+        if *current_value != new_value {
+            *current_value = new_value;
             true
         } else {
             false
@@ -69,30 +77,37 @@ where
 
 impl<T> State<T>
 where
-    T: Serialize
+    T: Serialize + Clone,
 {
     /// Publish the current state using the provided client.
     pub(crate) async fn publish(&self, client: &mut AsyncClient) -> anyhow::Result<()> {
         let mut payload_data = BytesMut::new().writer();
-        serde_json::to_writer(&mut payload_data, self.current())?;
+        // TODO figure out how to remove this clone
+        let value = self.current().await;
+        serde_json::to_writer(&mut payload_data, &value)?;
         client
             .publish_bytes(
                 self.topic(),
                 self.qos,
                 self.retain,
-        payload_data.into_inner().freeze()
+                payload_data.into_inner().freeze(),
             )
-            .await.map_err(anyhow::Error::from)
+            .await
+            .map_err(anyhow::Error::from)
     }
 }
 
 impl<T> State<T>
 where
-    T: fmt::Debug + PartialEq + Serialize
+    T: fmt::Debug + PartialEq + Serialize + Clone,
 {
     /// Combine [update] and [publish] into a single function.
-    pub(crate) async fn publish_if_update(&mut self, value: T, client: &mut AsyncClient) -> anyhow::Result<bool> {
-        if self.update(value) {
+    pub(crate) async fn publish_if_update(
+        &mut self,
+        value: T,
+        client: &mut AsyncClient,
+    ) -> anyhow::Result<bool> {
+        if self.update(value).await {
             self.publish(client).await.and(Ok(true))
         } else {
             debug!(value = ?self.value, "Skipping update of unchanged value");
@@ -103,11 +118,11 @@ where
 
 impl<T> fmt::Debug for State<T>
 where
-    T: fmt::Debug
+    T: fmt::Debug,
 {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("State")
-            .field("value", self.current())
+            .field("value", &self.value)
             .field("topic", &self.topic())
             .field("retain", &self.retain)
             .field("qos", &self.qos)
